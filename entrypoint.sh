82f4ecc69ff5e9942574ef032086e49222d95ee7
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-07-28 14:48:13 +0530

    action and entrypoint adjusted

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@
 cd /app && poetry install
 
 # Inputs arguments
-pr_number=$1
-draft=$2
-prerelease=$3
-generate_release_notes=$4
+token=$1
+pr_number=$2
+draft=$3
+prerelease=$4
+generate_release_notes=$5
+
+# Export GH_TOKEN so it can be used by other scripts/commands
+export GH_TOKEN="$token"
 
 # Build the command based on available parameters
 CMD="poetry run python3 /app/create_new_release.py --pr_number \"$pr_number\""
