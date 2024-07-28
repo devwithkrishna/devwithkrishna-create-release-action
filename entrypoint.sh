b2abcf3a43f5913620c7f1deb480b8edc7024526
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-07-28 14:32:42 +0530

    Entrypoint DEVOPS-265 added input args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,27 @@
 # Creating poetry venv
 cd /app && poetry install
 
+# Inputs arguments
+pr_number=$1
+draft=$2
+prerelease=$3
+generate_release_notes=$4
+
 # Build the command based on available parameters
-CMD="poetry run python3 /app/create_new_release.py"
+CMD="poetry run python3 /app/create_new_release.py --pr_number \"$pr_number\""
+
+
+if [ -n "$draft" ]; then
+    CMD="$CMD --draft \"$draft\""
+fi
+
+if [ -n "$prerelease" ]; then
+    CMD="$CMD --prerelease \"$prerelease\""
+fi
+
+if [ -n "$generate_release_notes" ]; then
+    CMD="$CMD --generate_release_notese \"$generate_release_notes\""
+fi
 
 # Print and execute the command
 echo "Executing command: $CMD"
